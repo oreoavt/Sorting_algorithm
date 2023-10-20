f917b8e55030da15447901eee74063d9fb76067b
oreoavt <bryanmongenavarro1@gmail.com>
2023-10-20 17:21:43 -0600

    Add counter

--- Insertion.java
+++ Insertion.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Insertion{
 	public static void insertionSort(int array[]){
 		int n = array.length;
@@ -12,18 +14,30 @@ public class Insertion{
 		}
 	}
 	public static void main(String a[]){
-		int[] arr1 = {9,14,3,2,43,11,58,22};
+		int[] arr1 = new int[100000];
+        Random rand = new Random();
+		for(int i=0; i< arr1.length; i++){
+			arr1[i] = rand.nextInt(100000);
+		}
+
 		System.out.println("Before insertion sort");
 		for(int i :arr1){
 			System.out.print(i+ " ");
 		}
 		System.out.println(" ");
 		System.out.println();
+
+		long startTime = System.currentTimeMillis();
+
 		insertionSort(arr1);
+
+		long endTime = System.currentTimeMillis();
 
 		System.out.println("After insertion sort");
 		for(int i:arr1){
 			System.out.print(i+" ");
 		}
+		long elapsedTime = endTime - startTime;
+		System.out.println("\nTiempo de ejecución: " + elapsedTime + " milisegundos");
 	}
 }
