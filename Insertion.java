693afde5b689d72e1e6617e5659e310489c327d4
oreoavt <bryanmongenavarro1@gmail.com>
2023-10-20 17:01:14 -0600

    Insertion practice

--- Insertion.java
+++ Insertion.java
@@ -1,5 +1,29 @@
 public class Insertion{
-	public static void main(String args[]){
-		System.out.print("Milton más shico");
+	public static void insertionSort(int array[]){
+		int n = array.length;
+		for(int j = 1; j < n; j++){
+			int key = array[j];
+			int i = j-1;
+			while((i > -1) && (array[i] > key)){
+				array[i+1] = array[i];
+				i--;
+			}
+			array[i+1] = key;
+		}
+	}
+	public static void main(String a[]){
+		int[] arr1 = {9,14,3,2,43,11,58,22};
+		System.out.println("Before insertion sort");
+		for(int i :arr1){
+			System.out.print(i+ " ");
+		}
+		System.out.println(" ");
+		System.out.println();
+		insertionSort(arr1);
+
+		System.out.println("After insertion sort");
+		for(int i:arr1){
+			System.out.print(i+" ");
+		}
 	}
 }
